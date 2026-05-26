39a4b5b8fc613338a21bcbcad28a342cce61d155
Shikhar Gupta <shikharguptah2@gmail.com>
2026-05-26 16:54:35 +0530

    Fix Prisma constructor validation error on build

--- lib/db.ts
+++ lib/db.ts
@@ -3,11 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: ["query"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
+    ...(process.env.DATABASE_URL ? {
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL
+        }
       }
-    }
+    } : {})
   })
 }
 
