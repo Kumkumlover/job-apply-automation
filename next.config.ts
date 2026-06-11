acd1fd99c7a343bf737ec8c2877b6fbae06561f0
Shikhar Gupta <shikharguptah2@gmail.com>
2026-06-11 20:25:49 +0530

    chore: ignore lint/ts errors on vercel builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
